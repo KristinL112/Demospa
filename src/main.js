56de341dfe70349b6b9b815ab3ce22019f84d7b5
Navigoo <...>
2024-02-01 20:01:00 +0100

    Adding rest

--- src/main.js
+++ src/main.js
@@ -1,23 +1,35 @@
-import './assets/base.css'
-
+// Importerar funktionen createApp från Vue för att skapa en Vue-applikation
 import { createApp } from 'vue'
+
+// Importerar funktionen createPinia från Pinia för att skapa ett state management system
 import { createPinia } from 'pinia'
 
-//Importera Bootstrap CSS stilar
+// Importerar Bootstrap CSS-stilar
 import 'bootstrap/dist/css/bootstrap.css'
 
-//Importera Bootstrap JS filer
+// Importerar Bootstrap JavaScript-filer
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
 
+// Importerar huvudkomponenten App.vue
 import App from './App.vue'
+
+// Importerar router-instansen från filen router.js
 import router from './router'
 
+// Importerar anpassade CSS-stilar från filen base.css
+import './assets/base.css'
+
+// Skapar en Vue-applikation
 const app = createApp(App)
 
+// Använder createPinia för att integrera Pinia i Vue-applikationen för globalt state-hantering
 app.use(createPinia())
+
+// Använder router-instansen för att lägga till router-funktionalitet i Vue-applikationen
 app.use(router)
 
-//För att importer ska fungera så behöver "använda dessa med ett middleware "use"
+// Använder Bootstrap-funktionalitet i Vue-applikationen
 app.use(bootstrap)
 
+// Monterar Vue-applikationen på elementet med id 'app' i HTML-dokumentet
 app.mount('#app')
